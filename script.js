8ad5e07b2c0565456d0564cc6301b7ead2bc5999
Halilwirasani23 <115181187+Halilwirasani23@users.noreply.github.com>
2024-09-24 10:59:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+const links = document.querySelectorAll('nav ul li a');
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
